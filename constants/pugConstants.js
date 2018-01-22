bae727d0a400a40c96d75f7e5130fcf3df2a76ef
andrewhalenkamp <andrew.halenkamp@centurylink.com>
2018-01-21 22:54:04 -0500

    updating gitignore and moving constants to own folder

---
+++ constants/pugConstants.js
@@ -0,0 +1,10 @@
+var pugConstants = {
+  INACTIVE: 'No Pug',
+  JOINING: 'Players are joining the pug',
+  FULL_NOT_READY: 'All pug slots taken, not all players ready',
+  PLAYERS_READY: 'All players are ready, wait for the host',
+  NA: 'North America',
+  EU: 'Europe'
+}
+
+module.exports = pugConstants;
